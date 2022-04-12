02fd08e073cbf828c8bec1f7b9e79b4b5529017e
MarkusTieger <77546847+MarkusTieger@users.noreply.github.com>
2022-04-12 21:07:19 +0200

    Update Bootstrap.java

--- src/main/java/de/MarkusTieger/Tigxa/Bootstrap.java
+++ src/main/java/de/MarkusTieger/Tigxa/Bootstrap.java
@@ -5,12 +5,16 @@ import javafx.stage.Stage;
 
 public class Bootstrap extends Application {
 
+    private static ClassLoader loader = null;
+    private static final String START_CLASS = "de.MarkusTieger.Tigxa.Bootstrap";
+
     public static void main(String[] args) {
+        loader = Thread.currentThread().getContextClassLoader();
         launch(args);
     }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Browser.start();
+        Class.forName(START_CLASS, true, loader);
     }
 }
