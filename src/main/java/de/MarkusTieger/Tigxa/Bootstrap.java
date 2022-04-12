d9f60fe20e2e5d08680102886aaf398b5db0a280
MarkusTieger <markustieger@gmail.com>
2022-04-12 23:42:18 +0200

    Removed Test-Code

--- src/main/java/de/MarkusTieger/Tigxa/Bootstrap.java
+++ src/main/java/de/MarkusTieger/Tigxa/Bootstrap.java
@@ -5,16 +5,12 @@ import javafx.stage.Stage;
 
 public class Bootstrap extends Application {
 
-    private static ClassLoader loader = null;
-    private static final String START_CLASS = "de.MarkusTieger.Tigxa.Bootstrap";
-
     public static void main(String[] args) {
-        loader = Thread.currentThread().getContextClassLoader();
         launch(args);
     }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Class.forName(START_CLASS, true, loader).getDeclaredMethod("start").invoke(null);
+        Browser.start();
     }
 }
